a241611380bd807f443dfd085c79cf8a16149453
Lam_Sam_glb <21657386@life.com>
2022-06-10 16:35:41 +0800

    master

---
+++ async+await/case.js
@@ -0,0 +1,21 @@
+
+        // const fs = require('fs');
+        // const util = require('util');
+        // const mineReadFile = util.promisify(fs.readFile);
+
+        // // async await
+        // async function main(){
+        //     try{
+        //         // 读取文件内容
+        //         let data1 = await mineReadFile('./a.html');
+        //         let data2 = await mineReadFile('./b.html');
+        //         let data3 = await mineReadFile('./c.html');
+        //         console.log(data1 + data2 + data3);
+        //     }catch(e){
+        //         console.log(e.code);
+        //     }
+        // }
+
+        // main();
+
+        
